b6bae81a6138f6c06a4cbdbaaa64602d3a128a36
ERFAN <erfan.aq0913@gmail.com>
2026-06-30 23:00:28 +0300

    fix:correct category gird layout by fixing data overlap and grid-rows config

--- app/Components/category/CategoryList.tsx
+++ app/Components/category/CategoryList.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import Button from "../button/Button";
+import db from "./../../../data/db.json";
 
-function CategoryList() {
+export interface TCategory {
+  id: string;
+  title: string;
+  subtitle: string;
+  image: string;
+  href: string;
+  colSpan: number;
+  rowSpan: number;
+  rowStart: number;
+  colStart: number;
+}
+
+async function CategoryList() {
+  const data = (await db.category) as TCategory[];
+
   return (
     <div>
       <div className="flex items-center justify-between gap-2 mx-5 my-5">
@@ -23,109 +38,37 @@ function CategoryList() {
         </Link>
       </div>
 
-      {/* ----------------------قسمت اسپورت */}
-      <div className="grid grid-cols-12 h-100 gap-5 items-center mx-5 ">
-        <div className="col-span-3 flex flex-col items-center relative   h-full rounded-2xl group overflow-hidden">
-          <Image
-            src="https://plus.unsplash.com/premium_photo-1668767725936-f896fbca8c66?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            fill
-            className="object-cover"
-          />
+      <div className="grid grid-cols-12 grid-rows-2 gap-5 mx-5 h-125 ">
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className="relative w-full h-full flex items-center flex-col rounded-2xl group overflow-hidden"
+            style={{
+              gridColumn: ` ${item.colStart} / span ${item.colSpan}`,
+              gridRow: ` ${item.rowStart} / span ${item.rowSpan}`,
+            }}
+          >
+            <Image
+              src={item.image}
+              alt={item.title}
+              fill
+              className="object-cover"
+            />
 
-          <div className="absolute bottom-12 right-4 text-right">
-            <div className="w-1 bg-white h-12 absolute -right-2 top-0"></div>
-            <h3 className="text-white font-black text-xl">کفش های ورزشی</h3>
-            <p className="text-white text-sm">مردانه و زنانه</p>
-          </div>
-          <Link
-            href="/category/sportShoes"
-            className="absolute -bottom-15 group-hover:bottom-3 transition-all duration-300"
-          >
-            <Button variant="success">مشاهده ای محصولات</Button>
-          </Link>
-        </div>
-        {/* ----------------------قسمت مجلسی زنانه و مردانه */}
-        <div className="col-span-5 h-full rounded-2xl flex flex-col gap-5 ">
-          <div className=" overflow-hidden relative flex-1 rounded-2xl group flex flex-col items-center">
-            <Image
-              src="https://media.istockphoto.com/id/1143262564/tr/foto%C4%9Fraf/parlak-%C5%9F%C4%B1k-stiletto-ayakkab%C4%B1-k%C4%B1z-bacaklar.jpg?s=1024x1024&w=is&k=20&c=KDDdI7OzDiikg07ySaDKvTlS_2fgiEXwmVPxOo7CHWU="
-              alt="Women's shoes"
-              fill
-              className="object-cover"
-            />
             <div className="absolute bottom-12 right-4 text-right">
-              <div className="w-1 h-12 bg-white absolute -right-2 top-0"></div>
-              <h3 className="text-white font-bold text-xl">کفش مجلسی</h3>
-              <p className="text-white ">زنانه</p>
+              <div className="w-1 bg-white h-12 absolute -right-2 top-0"></div>
+              <h3 className="text-white font-black text-xl">{item.title}</h3>
+              <p className="text-white text-sm">{item.subtitle}</p>
             </div>
+
             <Link
-              href="/category/wemanShoes"
-              className="absolute -bottom-15 group-hover:bottom-3  transition-all duration-300"
+              href={item.href}
+              className="absolute  -bottom-15  group-hover:bottom-3 transition-all duration-300"
             >
-              <Button variant="success">مشاهدای محصولات</Button>
+              <Button variant="success">مشاهده محصولات</Button>
             </Link>
           </div>
-          <div className=" overflow-hidden group relative flex-1 rounded-2xl flex flex-col items-center">
-            <Image
-              src="https://media.istockphoto.com/id/1061448006/tr/foto%C4%9Fraf/i%C5%9Fadam%C4%B1-giyim-ayakkab%C4%B1-i%C5%9F-i%C3%A7in-haz%C4%B1r-ba%C5%9Flarken-adam-sabah-d%C3%BC%C4%9F%C3%BCn-t%C3%B6reni-olmadan-damat.jpg?s=1024x1024&w=is&k=20&c=Q_LGjpPwxiR-6QfLXeuH9VGIpLK5rMoQj4cCcIyXokA="
-              alt="Men Shoes"
-              fill
-              className="object-cover"
-            />
-            <div className="absolute bottom-12 right-4 text-right">
-              <div className=" w-1 h-12 bg-white absolute -right-2 top-0"></div>
-              <h3 className="text-white font-bold text-xl">کفش مجلسی</h3>
-              <p className="text-white">مردانه</p>
-            </div>
-            <Link
-              href="/category/manShoes"
-              className="absolute -bottom-15 group-hover:bottom-3 transition-all duration-300"
-            >
-              <Button variant="success">مشاهده ای محصولات</Button>
-            </Link>
-          </div>
-        </div>
-        {/* کفش هلی بچگانه */}
-        <div className="col-span-2 h-full rounded-2xl  flex flex-col items-center overflow-hidden group relative">
-          <Image
-            src="https://plus.unsplash.com/premium_photo-1729104895296-343b3fcd4e35?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Children's shoes"
-            className="object-cover"
-            fill
-          />
-          <div className="absolute bottom-12 right-3">
-            <div className="w-1 h-12 bg-white absolute -right-2 top-0"></div>
-            <h3 className="text-white text-xl font-bold">کفش بچگانه</h3>
-            <p className="text-white text-sm">پسرانه و دخترانه</p>
-          </div>
-          <Link
-            href="/category/kidsShoes"
-            className="absolute -bottom-15 group-hover:bottom-3 transition-all duration-300"
-          >
-            <Button variant="success">مشاهده ای محصولات</Button>
-          </Link>
-        </div>
-        {/* کفش هلی سندل */}
-        <div className="col-span-2 flex flex-col items-center bg-green-500 h-full rounded-2xl group overflow-hidden relative">
-          <Image
-            src="https://images.unsplash.com/photo-1603487742131-4160ec999306?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="sandals"
-            fill
-            className="object-cover"
-          />
-          <div className="absolute bottom-12 right-4">
-            <div className="w-1 h-12 absolute bg-white -right-2 top-0"></div>
-            <h3 className="text-white font-bold text-xl">سندل های تابستانه</h3>
-            <p className="text-white text-sm">مردانه و زنانه</p>
-          </div>
-          <Link
-            href="/category/sandalsShoes"
-            className=" absolute -bottom-15 group-hover:bottom-3 transition-all duration-300"
-          >
-            <Button variant="success">مشاهدای محصولات</Button>
-          </Link>
-        </div>
+        ))}
       </div>
     </div>
   );
