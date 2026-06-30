115926c1e47ce8ffa94bcbd6a9a9a2eb46401e1c
ERFAN <erfan.aq0913@gmail.com>
2026-07-01 00:58:19 +0300

    feat :add reusable sectionHeader component

--- app/Components/category/CategoryList.tsx
+++ app/Components/category/CategoryList.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "../button/Button";
 import db from "./../../../data/db.json";
+import SectionHeader from "../sectionHeader/SectionHeader";
 
 export interface TCategory {
   id: string;
@@ -20,23 +21,7 @@ async function CategoryList() {
 
   return (
     <div>
-      <div className="flex items-center justify-between gap-2 mx-5 my-5">
-        <h2 className="text-xl font-bold text-emerald-600">دسته بندی</h2>
-        <div className="h-0.5  bg-gray-200 flex-1"></div>
-        <Link href="/category" className="flex items-center">
-          <p>مشاهده همه</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            className="bi bi-arrow-left-circle-fill text-emerald-300 hover:text-emerald-500 transition-all duration-300"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
-          </svg>
-        </Link>
-      </div>
+      <SectionHeader title=" دسته بندی " href="/category" />
 
       <div className="grid grid-cols-12 grid-rows-2 gap-5 mx-5 h-125 ">
         {data.map((item) => (
